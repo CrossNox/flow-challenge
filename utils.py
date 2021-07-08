42e8654bd8b53ffa85808613504ff338027bb889
CrossNox <ijmermet@gmail.com>
2021-07-07 22:29:27 -0300

    MAP, type hints and docstrings

--- utils.py
+++ utils.py
@@ -1,5 +1,43 @@
 from sklearn.model_selection import train_test_split
+from typing import Sequence, Tuple
+import numpy as np
+import pandas as pd
 
-def sample_users(df, prop=0.2):
+def sample_users(df: pd.DataFrame, prop:float=0.2) -> Tuple[pd.DataFrame, pd.DataFrame]:
+    """Sample users from df.
+    
+    Parameters
+    ----------
+      df: Dataframe with column "account_id".
+      prop: Proportion between 0 and 1 to sample.
+      
+    Returns
+    -------
+      train, test: Train and test sets by account id.
+    """
     train_users, test_users = train_test_split(df.account_id.unique(), test_size=prop)
     return df[df.account_id.isin(set(train_users))], df[df.account_id.isin(set(test_users))]
+
+def MAP(y: Sequence[Sequence[int]], yhat: Sequence[Sequence[int]]) -> float:
+    """Compute MAP.
+    
+    Parameters
+    ----------
+      y: True labels.
+      yhat: predictions.
+    
+    Returns
+    -------
+      MAP score.
+    """
+    def AP(y: Sequence[int], yhat: Sequence[int]):
+        """Compute AP."""
+        n = len(yhat)
+        arange = np.arange(1, n+1, dtype=np.int32)
+        rel_k = np.in1d(yhat[:n], y)
+        tp = np.ones(rel_k.sum(), dtype=np.int32).cumsum()
+        denom = arange[rel_k]
+        ap = (tp / denom).sum() / len(y)
+        return ap
+    
+    return np.mean([AP(_y, _yhat) for _y, _yhat in zip(y, yhat)])
